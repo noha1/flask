9fb0eccdad99d81758cf2be95cd420415f3ce4fe
noha <noha00@hotmail.com>
2022-10-06 13:06:17 +0100

    new: include jinja concept

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-import flask from Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+blog = {
+    'name': 'My awesome blog',
+    'posts': {
+        0: {
+            'title': " my first post",
+            'content': 'content is here',
+        }
+    }
+}
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', blog=blog)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = blog['posts'].get(post_id)
+    print(post)
+    if not post:
+        return render_template('404.html', message=f'A post with id {post_id} was not found.')
+
+    return render_template('post.html', post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
